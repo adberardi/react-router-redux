1df9a6789465a051ed1c806387cf0c13e6d95d59
Antonio Berardi <adberardi@yahoo.com>
2021-08-25 13:39:29 -0400

    Clase 4: Container: Login

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch, NavLink, Link, BrowserRouter } from 'react-router-dom';
 import Home from '../containers/Home';
+import Login from '../containers/Login';
 
 
 const App = () => (
     <BrowserRouter>
     <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
     </BrowserRouter>
 );
 
