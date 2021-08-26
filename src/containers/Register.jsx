f1a093fd8bd7714d3307e2174223599a4f79dc0d
Antonio Berardi <adberardi@yahoo.com>
2021-08-26 14:57:33 -0400

    Clase 8 - Manejando enlaces y configuraciones

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/styles/components/Register.scss';
 
 const Register = () => (
-<section class="register">
-    <section class="register__container">
+<section className="register">
+    <section className="register__container">
       <h2>Regístrate</h2>
-      <form class="register__container--form">
-        <input class="input" type="text" placeholder="Nombre" />
-        <input class="input" type="text" placeholder="Correo" />
-        <input class="input" type="password" placeholder="Contraseña" />
-        <button class="button">Registrarme</button>
+      <form className="register__container--form">
+        <input className="input" type="text" placeholder="Nombre" />
+        <input className="input" type="text" placeholder="Correo" />
+        <input className="input" type="password" placeholder="Contraseña" />
+        <button className="button">Registrarme</button>
       </form>
-      <a href="">Iniciar sesión</a>
+      <Link to="/login">Iniciar sesión</Link>
     </section>
   </section>
 );
