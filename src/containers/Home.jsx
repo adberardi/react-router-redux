455e434b398ddeed58f851086c2659d72ad72306
Antonio Berardi <adberardi@yahoo.com>
2021-08-26 14:43:54 -0400

    Clase 7 - Container Layout

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
+
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
+
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
@@ -14,7 +14,7 @@ const Home = () => {
   const initialState = useInitialState(API);
   return initialState.length === 0 ? <h1>Loading...</h1> : (
     <div className="App">
-      <Header />
+      <>
       <Search />
       {initialState.mylist.length > 0 &&
         <Categories title="Mi Lista">
@@ -39,7 +39,7 @@ const Home = () => {
           )}
         </Carousel>
       </Categories>
-      <Footer />
+      </>
     </div>
   );
 }
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Route, Switch, NavLink, Link, BrowserRouter } from 'react-router-dom';
-import Home from '../containers/Home';
-import Login from '../containers/Login';
-import Register from '../containers/Register';
-import NotFound from '../containers/NotFound';
-
+import React from "react";
+import { Route, Switch, NavLink, Link, BrowserRouter } from "react-router-dom";
+import Home from "../containers/Home";
+import Login from "../containers/Login";
+import Register from "../containers/Register";
+import NotFound from "../components/Layout";
+import Layout from "../components/Layout";
 
 const App = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register}/>
-            <Route component={NotFound} />
-        </Switch>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
 );
 
 export default App;
-
-
