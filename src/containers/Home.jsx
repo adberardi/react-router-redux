60c28363844da0e63e3aba6a0eeffacd11df154b
Antonio Berardi <adberardi@yahoo.com>
2021-08-27 12:52:04 -0400

    Clase 12 - Creando el Store

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,46 +4,54 @@ import Search from "../components/Search";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
-
+import { connect } from "react-redux";
 import useInitialState from "../hooks/useInitialState";
 import "../assets/styles/App.scss";
 
-const API = "http://localhost:3000/initalState";
+// const API = "http://localhost:3000/initalState";
 
-const Home = () => {
-  const initialState = useInitialState(API);
-  return initialState.length === 0 ? (
-    <h1>Loading...</h1>
-  ) : (
-    <div className="App">
-      <>
-        <Search />
-        {initialState.mylist.length > 0 && (
-          <Categories title="Mi Lista">
-            <Carousel>
-              {initialState.mylist.map((item) => (
-                <CarouselItem key={item.id} {...item} />
-              ))}
-            </Carousel>
-          </Categories>
-        )}
-        <Categories title="Tendencias">
-          <Carousel>
-            {initialState.trends.map((item) => (
-              <CarouselItem key={item.id} {...item} />
-            ))}
-          </Carousel>
-        </Categories>
-        <Categories title="Originales de Platzi Video">
-          <Carousel>
-            {initialState.originals.map((item) => (
-              <CarouselItem key={item.id} {...item} />
-            ))}
-          </Carousel>
-        </Categories>
-      </>
-    </div>
-  );
+const Home = ({ myList, trends, originals }) => {
+  // const initialState = useInitialState(API);
+  return(
+  <>
+    <Search />
+    {myList.length > 0 && (
+      <Categories title="Mi Lista">
+        <Carousel>
+          {myList.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+    )}
+    <Categories title="Tendencias">
+      <Carousel>
+        {trends.map((item) => (
+          <CarouselItem key={item.id} {...item} />
+        ))}
+      </Carousel>
+    </Categories>
+    <Categories title="Originales de Platzi Video">
+      <Carousel>
+        {originals.map((item) => (
+          <CarouselItem key={item.id} {...item} />
+        ))}
+      </Carousel>
+    </Categories>
+  </>);
 };
 
-export default Home;
+/**
+ *
+ * @param {*} state Los valores pasados por referencia desde el primer nivel por medio del store ( consultar variable initialState en src/index.js)
+ * @returns Los valores a ser usados en este componente
+ */
+const mapStateToProps = (state) => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
