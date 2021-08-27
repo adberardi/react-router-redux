76afb24ed27e355d710bd556b891b1bb70af842c
Antonio Berardi <adberardi@yahoo.com>
2021-08-27 15:48:36 -0400

    Clase 16 - Crear Formulario de Login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Login.scss';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
-const Login = () => (
+const Login = () => {
+    const [form, setValues] = useState({
+        email: "",
+    });
+
+    const handleInput = (event) => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value,
+        })
+    };
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(form);
+    }
+
+    return (
     <section className="login">
         <section className="login__container">
             <h2>Inicia sesión</h2>
-            <form className="login__container--form">
-                <input className="input" type="text" placeholder="Correo" />
-                <input className="input" type="password" placeholder="Contraseña" />
-                <button className="button">Iniciar sesión</button>
+            <form className="login__container--form" onSubmit={handleSubmit}>
+                <input className="input" type="text" name="email" placeholder="Correo" onChange={handleInput} />
+                <input className="input" type="password" name="passw" placeholder="Contraseña" onChange={handleInput} />
+                <button className="button" type="submit" >Iniciar sesión</button>
                 <div className="login__container--remember-me">
                     <label>
                         <input type="checkbox" id="cbox1" value="first_checkbox" />
@@ -26,7 +44,9 @@ const Login = () => (
             </section>
             <p className="login__container--register">No tienes ninguna cuenta <Link to="/register">Regístrate</Link></p>
         </section>
-    </section>);
+    </section>
+    )
+    };
 
 
 export default Login;
