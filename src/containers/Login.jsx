b9eac87689236bcd280f0229eb9beddc4ced27b4
Antonio Berardi <adberardi@yahoo.com>
2021-08-27 16:04:52 -0400

    Formulario de Login con Redux

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,52 +1,78 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../assets/styles/components/Login.scss';
-import googleIcon from '../assets/static/google-icon.png';
-import twitterIcon from '../assets/static/twitter-icon.png';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { loginRequest } from "../actions";
+import "../assets/styles/components/Login.scss";
+import googleIcon from "../assets/static/google-icon.png";
+import twitterIcon from "../assets/static/twitter-icon.png";
 
-const Login = () => {
-    const [form, setValues] = useState({
-        email: "",
+const Login = (props) => {
+  const [form, setValues] = useState({
+    email: "",
+  });
+
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
     });
+  };
 
-    const handleInput = (event) => {
-        setValues({
-            ...form,
-            [event.target.name]: event.target.value,
-        })
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+    props.loginRequest(form);
+    props.history.push("/");
+  };
 
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(form);
-    }
-
-    return (
+  return (
     <section className="login">
-        <section className="login__container">
-            <h2>Inicia sesión</h2>
-            <form className="login__container--form" onSubmit={handleSubmit}>
-                <input className="input" type="text" name="email" placeholder="Correo" onChange={handleInput} />
-                <input className="input" type="password" name="passw" placeholder="Contraseña" onChange={handleInput} />
-                <button className="button" type="submit" >Iniciar sesión</button>
-                <div className="login__container--remember-me">
-                    <label>
-                        <input type="checkbox" id="cbox1" value="first_checkbox" />
-                        Recuérdame
-                    </label>
-                    <a href="/">Olvidé mi contraseña</a>
-                </div>
-            </form>
-            <section className="login__container--social-media">
-                <div><img src={googleIcon} /> Inicia sesión con Google</div>
-                <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
-            </section>
-            <p className="login__container--register">No tienes ninguna cuenta <Link to="/register">Regístrate</Link></p>
+      <section className="login__container">
+        <h2>Inicia sesión</h2>
+        <form className="login__container--form" onSubmit={handleSubmit}>
+          <input
+            className="input"
+            type="text"
+            name="email"
+            placeholder="Correo"
+            onChange={handleInput}
+          />
+          <input
+            className="input"
+            type="password"
+            name="passw"
+            placeholder="Contraseña"
+            onChange={handleInput}
+          />
+          <button className="button" type="submit">
+            Iniciar sesión
+          </button>
+          <div className="login__container--remember-me">
+            <label>
+              <input type="checkbox" id="cbox1" value="first_checkbox" />
+              Recuérdame
+            </label>
+            <a href="/">Olvidé mi contraseña</a>
+          </div>
+        </form>
+        <section className="login__container--social-media">
+          <div>
+            <img src={googleIcon} /> Inicia sesión con Google
+          </div>
+          <div>
+            <img src={twitterIcon} /> Inicia sesión con Twitter
+          </div>
         </section>
+        <p className="login__container--register">
+          No tienes ninguna cuenta <Link to="/register">Regístrate</Link>
+        </p>
+      </section>
     </section>
-    )
-    };
+  );
+};
 
+const mapDispatchToProps = {
+  loginRequest,
+};
 
-export default Login;
+export default connect(null, mapDispatchToProps)(Login);
