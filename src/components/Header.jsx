a532f34d9db15b8de6dfd3fa89458d45bb34e5d4
Antonio Berardi <adberardi@yahoo.com>
2021-08-30 14:34:06 -0400

    Clase 19: Uso  de Gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,27 +3,44 @@ import "../assets/styles/components/Header.scss";
 import logo from "../assets/static/logo-platzi-video-BW2.png";
 import userIcon from "../assets/static/user-icon.png";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import gravatar from "../utils/gravatar";
 
-const Header = () => (
-  <header className="header">
-    <Link to="/">
-      <img className="header__img" src={logo} alt="Platzi Video" />
-    </Link>
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="" />
-        <p>Perfil</p>
+const Header = (props) => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
+  return (
+    <header className="header">
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {hasUser ? (
+            <img src={gravatar(user.email)} alt={user.email} />
+          ) : (
+            <img src={userIcon} alt="" />
+          )}
+
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <Link to="/login">Cuenta</Link>
+          </li>
+          <li>
+            <Link to="/">Cerrar Sesión</Link>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <Link to="/login">Cuenta</Link>
-        </li>
-        <li>
-          <Link to="/">Cerrar Sesión</Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps, null)(Header);
