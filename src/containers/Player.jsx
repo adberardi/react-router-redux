f4d9d957c209c5948838ddd1006952b712438ffc
Antonio Berardi <adberardi@yahoo.com>
2021-08-31 12:29:19 -0400

    Clase 24 - Arreglando la funcionalidad del player

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "../assets/styles/components/Player.scss";
 
-const Player = () => {
+const Player = props => {
+
+    const { id } = props.match.params;
+
   return (
     <div className="Player">
       <video controls autoPlay>
         <source src="" type="video/mp4" />
       </video>
-      <div className="Player-black">
+      <div className="Player-back" onClick={() => props.history.goBack()}>
         <button type="button">Regresar</button>
       </div>
     </div>
